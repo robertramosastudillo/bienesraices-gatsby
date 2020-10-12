6721af3fa42d27812c3355ef04ee2d7670aaedce
robertramosastudillo <robertramosastudillo@gmail.com>
2020-10-12 17:16:23 -0500

    Inicio de proyecto

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 
-const Index = () => {
+const Nosotros = () => {
   return (
     <Layout>
-      <h1>Index</h1>
+      <h1>Nosotros</h1>
     </Layout>
   )
 }
 
-export default Index
+export default Nosotros
