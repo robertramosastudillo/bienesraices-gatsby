893741bcfd794fde4df7b7250d74695e0e406c8b
robertramosastudillo <robertramosastudillo@gmail.com>
2020-10-18 09:44:59 -0500

    Se trabajo en la pagina de inicio

---
+++ src/hooks/usePropiedades.js
@@ -0,0 +1,38 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const usePropiedades = () => {
+  const datos = useStaticQuery(graphql`
+    query {
+      allStrapiPropiedades {
+        nodes {
+          nombre
+          descripcion
+          id
+          wc
+          precio
+          estacionamiento
+          habitaciones
+          categoria {
+            nombre
+          }
+          agentes {
+            nombre
+            telefono
+            email
+          }
+          imagen {
+            sharp: childImageSharp {
+              fluid(maxWidth: 600, maxHeight: 400) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(datos)
+}
+
+export default usePropiedades
